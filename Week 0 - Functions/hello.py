ad9fb81cf7de33710714d557442b54273ede41e0
Khang Hoang <khangben367@gmail.com>
2026-04-18 22:23:39 -0700

    updated hello file

--- Week 0 - Functions/hello.py
+++ Week 0 - Functions/hello.py
@@ -1,6 +1,11 @@
 
-# Ask user for their name
-name = input("What's your name? ")
+# Ask user for their name, remove whitespace from str, title user's first and last name
+name = input("What's your name? ").strip().title()
 
 # say hello to user
-print(f"Hello, {name}")
+print(f"hello, {name}")
+
+# print("Hello, \"friend\"")
+
+# print("Hello, ", end="")
+# print(name)
